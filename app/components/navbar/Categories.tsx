6884cb408d7140b73b7a2f6f564ff0cc026759f8
Abhilasha Purwar <abhilasha.purwar@gmail.com>
2024-04-08 14:38:55 +0530

    Category box

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,7 +1,74 @@
-import React from "react";
+"use client";
+
+import { usePathname, useSearchParams } from "next/navigation";
+import Container from "../Container";
+import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
+import { GiWindmill } from "react-icons/gi";
+import { MdOutlineVilla } from "react-icons/md";
+
+import CategoryBox from "../CategoryBox";
+import { useRouter } from "next/router";
+
+export const categories = [
+  {
+    label: "Wishlist",
+    icon: TbBeach,
+    description: "This is my WishList!",
+  },
+  {
+    label: "Order",
+    icon: GiWindmill,
+    description: "These are my Current Orders",
+  },
+  {
+    label: "Wardrobe",
+    icon: MdOutlineVilla,
+    description: "These items are in my Wardrobe!",
+  },
+  {
+    label: "Vanity",
+    icon: TbMountain,
+    description: "Skincare, Haircare, Makeup, and more!",
+  },
+  {
+    label: "Accessories",
+    icon: TbPool,
+    description: "Bags, Shoes, and Jewellery!",
+  },
+];
 
 const Categories = () => {
-  return <div>Categories</div>;
+  const params = useSearchParams();
+  const category = params?.get("category");
+  const pathname = usePathname();
+  const isMainPage = pathname === "/";
+  if (!isMainPage) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <div
+        className="
+      flex
+      flex-row
+      items-center
+      justify-between
+      overflow-x-auto
+      "
+      >
+        {categories.map((item) => (
+          <CategoryBox
+            key={item.label}
+            label={item.label}
+            icon={item.icon}
+            selected={category === item.label}
+            //onClick={() => router.push(`/?category=${item.label}`)}
+          />
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default Categories;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import Container from "./components/Container";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Hello Wishlist
+    <Container>
+      <div className="flex">
+        <div className="pt-48 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 2xl:grid-cols-7 gap-10">
+          <div className="text-rose-500 text-2xl gap-10"> </div>
+        </div>
       </div>
-    </main>
+    </Container>
   );
 }
